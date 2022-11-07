e1e86ed97b8d4c5ec956c2b2cdb64aed5a330df8
Yousif Akbar <11247449+yhakbar@users.noreply.github.com>
2022-11-07 16:03:07 -0500

    Formatting

--- lambda.tf
+++ lambda.tf
@@ -7,24 +7,24 @@ module "lambda" {
   runtime  = var.runtime
 
   # Optional
-  name                    = local.lambda_name
-  description             = var.lambda_description
-  role_arn                = var.role_arn
-  timeout                 = var.timeout
-  environment_vars        = var.environment_vars
-  memory_size             = var.memory_size
-  log_retention_in_days   = var.log_retention_in_days
-  publish                 = var.publish
-  policy_json             = var.policy_json
-  layers                  = var.layers
-  tracing_config_mode     = var.tracing_config_mode
-  use_prefix              = var.use_prefix
-  architectures           = var.architectures
-  file_system_config      = var.file_system_config
-  vpc_id                  = var.vpc_id
-  add_vpc_config          = var.add_vpc_config
-  security_group_id       = var.security_group_id
-  subnets                 = var.subnets
+  name                  = local.lambda_name
+  description           = var.lambda_description
+  role_arn              = var.role_arn
+  timeout               = var.timeout
+  environment_vars      = var.environment_vars
+  memory_size           = var.memory_size
+  log_retention_in_days = var.log_retention_in_days
+  publish               = var.publish
+  policy_json           = var.policy_json
+  layers                = var.layers
+  tracing_config_mode   = var.tracing_config_mode
+  use_prefix            = var.use_prefix
+  architectures         = var.architectures
+  file_system_config    = var.file_system_config
+  vpc_id                = var.vpc_id
+  add_vpc_config        = var.add_vpc_config
+  security_group_id     = var.security_group_id
+  subnets               = var.subnets
 
   permissions_boundary_arn                        = var.permissions_boundary_arn
   lambda_insights_extension_version               = var.lambda_insights_extension_version
