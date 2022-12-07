000bbf2e867fea8e9645478c6d6e34fac8fb9309
Yousif Akbar <11247449+yhakbar@users.noreply.github.com>
2022-12-07 08:53:13 -0500

    Also logging into public ECR before starting tests

--- scripts/test.sh
+++ scripts/test.sh
@@ -5,7 +5,8 @@ set -euo pipefail
 IFS=$'\n\t'
 
 # We have to log into ECR now that we're doing Lambdas with containers
-aws ecr get-login-password --region 'us-east-1' | docker login --username AWS --password-stdin "$(aws sts get-caller-identity | jq -r '.Account').dkr.ecr.us-east-1.amazonaws.com"
+aws ecr-public get-login-password --region us-east-1 | docker login --username AWS --password-stdin public.ecr.aws >/dev/null
+aws ecr get-login-password --region 'us-east-1' | docker login --username AWS --password-stdin "$(aws sts get-caller-identity | jq -r '.Account').dkr.ecr.us-east-1.amazonaws.com" >/dev/null
 
 GIT_ROOT=$(git rev-parse --show-toplevel)
 pushd "$GIT_ROOT"/tests >/dev/null || exit 1
